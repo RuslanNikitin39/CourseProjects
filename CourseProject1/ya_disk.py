843ba74b455d8231c5adbc3976c0f304b22fed3f
RuslanNikitin39 <ruslan-nikitin@mail.ru>
2021-12-12 13:34:12 +0200

    added metods mkdir, exist, upload_url

--- CourseProject1/ya_disk.py
+++ CourseProject1/ya_disk.py
@@ -7,6 +7,7 @@ class YandexDisk:
     def get_headers(self):
         return {
             'Content-Type': 'application/json',
+            'Accept': 'application/json',
             'Authorization': 'OAuth {}'.format(self.token)
         }
 
@@ -25,3 +26,24 @@ class YandexDisk:
         if response.status_code == 201:
             print("Success")
 
+    def upload_url(self, url: str, file_path: str):
+        up_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        params = {'url': url,'path': file_path, 'disable_redirects': True}
+        response = requests.post(up_url, headers=self.get_headers(), params=params)
+        response.raise_for_status()
+        if response.status_code == 202:
+            return response
+
+    def mkdir(self, path: str):
+        """Создание папки. path: Путь к создаваемой папке."""
+        url = 'https://cloud-api.yandex.net/v1/disk/resources'
+        requests.put(f'{url}?path={path}', headers=self.get_headers())
+
+    def exists(self, path):
+        """ Проверка существования папки. path: Путь к проверяемой папке."""
+        url = 'https://cloud-api.yandex.net/v1/disk/resources'
+        response = requests.get(f'{url}?path={path}', headers=self.get_headers())
+        if response.status_code == 200:
+            return True
+        else:
+            return False
